6aa1c7976c15e39de2504d307f350bc3258b90c9
Rich Evans <rcb.evans@outlook.com>
2017-07-25 00:01:52 -0700

    Update to Python3 and add gitignore

--- model_builder.py
+++ model_builder.py
@@ -7,21 +7,21 @@ from keras.layers.core import Dense, Dropout
 # This is abstract class. You need to implement yours.
 class AbstractModelBuilder(object):
 
-	def __init__(self, weights_path = None):
-		self.weights_path = weights_path
+    def __init__(self, weights_path = None):
+        self.weights_path = weights_path
 
-	def getModel(self):
-		weights_path = self.weights_path
-		model = self.buildModel()
+    def getModel(self):
+        weights_path = self.weights_path
+        model = self.buildModel()
 
-		if weights_path and path.isfile(weights_path):
-			try:
-				model.load_weights(weights_path)
-			except Exception, e:
-				print e
+        if weights_path and path.isfile(weights_path):
+            try:
+                model.load_weights(weights_path)
+            except Exception as e:
+                print(e)
 
-		return model
+        return model
 
-	# You need to override this method.
-	def buildModel(self):
-		raise NotImplementedError("You need to implement your own model.")
+    # You need to override this method.
+    def buildModel(self):
+        raise NotImplementedError("You need to implement your own model.")
